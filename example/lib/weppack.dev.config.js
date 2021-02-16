ba50947047d0ce68c4b631b147db4ce79ff644d5
santiloopz <santilpzate@gmail.com>
2021-02-16 07:51:09 +0100

    WIP prepare workspace

--- example/lib/weppack.dev.config.js
+++ example/lib/weppack.dev.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -9,4 +10,18 @@ module.exports = {
 		filename: '[name].js',
 		path: path.resolve(__dirname, './dist'),
 	},
+	module: {
+		rules: [
+			{ test: /\.hbs$/, use: 'handlebars-loader', }
+		],
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './src/index.hbs',
+			filename: 'index.html',
+			title: 'Processing | Mouse Interaction',
+			title: 'A simple example, just to get started',
+		}),
+		// TODO: implement clean webpack plugin
+	],
 };
